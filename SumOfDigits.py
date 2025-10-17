3d1f017f063e238bc62d83748c82bd6c67637f2f
girisa06 <gmgirisamurali@gmail.com>
2025-10-17 19:07:43 +0530

    Update SumOfDigits.py

--- SumOfDigits.py
+++ SumOfDigits.py
@@ -1,5 +1,6 @@
-N = str(input("Enter a number: ")).strip()
+N = str(input("Enter a number greater than 9: ")).strip()
 sum = 0
 for i in N:
     sum += int(i)
+
 print("Sum of digits in integer N = " + str(sum))
